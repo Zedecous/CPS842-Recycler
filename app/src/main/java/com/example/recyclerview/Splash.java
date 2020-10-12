0b9834c684d30b6f6ae7b5de92296f864b2fb1b2
Zedecous <swatenya@gmail.com>
2020-10-12 10:30:58 -0400

    Added audio to splash screen

--- app/src/main/java/com/example/recyclerview/Splash.java
+++ app/src/main/java/com/example/recyclerview/Splash.java
@@ -3,6 +3,7 @@ package com.example.recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
@@ -11,12 +12,17 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        MediaPlayer md = new MediaPlayer();
+        md = MediaPlayer.create(this,R.raw.dna);
+        final MediaPlayer finalMd = md;
         Thread background = new Thread() {
             public void run() {
                 try {
+
+                    finalMd.start();
                     // Thread will sleep for 5 seconds
                     sleep(5*1000);
-
+                    finalMd.stop();
                     // After 5 seconds redirect to another intent
                     Intent i=new Intent(getBaseContext(),RecipeListActivity.class);
                     startActivity(i);
